48a5bb509bdc6f49d83d80cc1e979bce23cb2e4c
codrelphi <chancerel.codjovi@gmail.com>
2022-01-26 20:18:59 +0100

    ajouter le model User

---
+++ produitAppV2/src/fr/produitappv2/models/User.java
@@ -0,0 +1,74 @@
+package fr.produitappv2.models;
+
+public class User {
+
+	private int id;
+	private String nom;
+	private String prenom;
+	private char sex;
+	private String login;
+	private String password;
+	
+	public User() {}
+
+	public User(String nom, String prenom, char sex, String login, String password) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.sex = sex;
+		this.login = login;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	private void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public char getSex() {
+		return sex;
+	}
+
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+}
